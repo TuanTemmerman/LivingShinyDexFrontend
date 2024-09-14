0cb493d988cdf8f13ff5d481ac5523700e3665f1
TuanTemmerman <temmermantuan@hotmail.com>
2024-09-14 14:45:19 +0200

    basic styling added

--- livingdex/src/app/app.routes.ts
+++ livingdex/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { RouterModule, Routes } from '@angular/router';
-import { PokemonComponent } from './Components/pokemon/pokemon.component';
 import { NgModule } from '@angular/core';
 import { PokemonListComponent } from './Components/pokemon-list/pokemon-list.component';
 import { PokemonMyListComponent } from './Components/pokemon-my-list/pokemon-my-list.component';
